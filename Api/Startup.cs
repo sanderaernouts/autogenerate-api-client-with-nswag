192d5166a4cbf3bac572ab44ce4dbfc0fece3f90
Sander Aernouts <sander.aernouts@gmail.com>
2020-10-25 14:54:55 +0100

    upgraded to ASP.net core 3.1, moved client generation to Api project, and added output logging to MSBuild

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NJsonSchema;
-using NSwag;
-using NSwag.AspNetCore;
 
 namespace Example.Api
 {
