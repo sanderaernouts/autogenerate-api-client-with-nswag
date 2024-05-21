6cb4d3fb799b3476aea3c39ee533faa089b09964
Sander Aernouts <sander.aernouts@gmail.com>
2024-05-21 16:38:07 +0200

    Updated to .NET 8.0

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,20 +1,42 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Example.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+                
+
+builder.Services.AddOpenApiDocument(settings =>
+{
+    settings.PostProcess = document =>
+    {
+        document.Info.Version = "v1";
+        document.Info.Title = "Example API";
+        document.Info.Description = "REST API for example.";
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+if(!app.Environment.IsProduction()) {
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
+app.MapControllers();
+
+app.Run();
